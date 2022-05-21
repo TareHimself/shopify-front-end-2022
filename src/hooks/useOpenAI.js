2b4a580f776abba841187580f07fc9a490f956cc
Tare Ebelo <75279482+TareHimself@users.noreply.github.com>
2022-05-20 23:18:27 -0400

    created project

---
+++ src/hooks/useOpenAI.js
@@ -0,0 +1,8 @@
+import { useCallback, useEffect } from "react";
+
+export default function useOpenAI(engine = "text-curie-001") {
+    const sendPrompt = useCallback((prompt) => {
+
+    })
+    useEffect
+}
